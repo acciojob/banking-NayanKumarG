74e51fcc8a4a31a3bfe7b6ecbea27e27af0473e9
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-08-25 14:15:04 +0530

    Asiignment submission

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -36,39 +36,39 @@ public class CurrentAccount extends BankAccount {
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
         String s = this.tradeLicenseId;
-        if(isValid(this.tradeLicenseId)) return;
-        HashMap<Character, Integer> map = new HashMap<>();
+        if(!isNumberValid(this.tradeLicenseId)) {
+            HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
-
-        for (Map.Entry<Character, Integer> e : map.entrySet()) {
-            pq.add(new Pair(e.getKey(), e.getValue()));
-        }
-
-        Pair prev = new Pair('#', -1);
-
-        StringBuilder sb = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-            Pair curr = pq.poll();
-            sb.append(curr.ch);
-            curr.freq--;
-
-            if (prev.freq > 0) {
-                pq.add(prev);
+            for (int i = 0; i < s.length(); i++) {
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
             }
 
-            prev = curr;
-        }
+            PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
 
-        if (sb.length() != s.length()) throw new Exception("Valid License can not be generated");
-        this.tradeLicenseId = sb.toString();
+            for (Map.Entry<Character, Integer> e : map.entrySet()) {
+                pq.add(new Pair(e.getKey(), e.getValue()));
+            }
+
+            Pair prev = new Pair('#', -1);
+
+            StringBuilder sb = new StringBuilder();
+
+            while (!pq.isEmpty()) {
+                Pair curr = pq.poll();
+                sb.append(curr.ch);
+                curr.freq--;
+
+                if (prev.freq > 0) {
+                    pq.add(prev);
+                }
+
+                prev = curr;
+            }
+
+            if (sb.length() != s.length()) throw new Exception("Valid License can not be generated");
+            this.tradeLicenseId = sb.toString();
 //            return sb.toString();
-
+        }
     }
     public boolean isNumberValid(String licenseId){
         for(int i=0; i<licenseId.length()-1; i++){
